cb9d0097c846fc5bfbc28c4508ed8f1e1383b441
ycui10 <60239373+ycui10@users.noreply.github.com>
2020-04-27 15:13:05 -0400

    second commit

--- model.py
+++ model.py
@@ -6,12 +6,7 @@ import numpy as np
 import image
 from datetime import datetime
 from tensorflow.keras.models import Model
-# tf.enable_eager_execution(
-#     config=None,
-#     device_policy=None,
-#     execution_mode=None
-# )
-                               
+                          
 
 content_layers = ['block4_conv2']
 style_layers = ['block1_conv1',
@@ -35,13 +30,17 @@ def model_init():
     
     vgg19 = tf.keras.applications.VGG19(include_top = False,weights = 'imagenet')
     vgg19.trainable = False
+    content_output = []
+    style_output = []
+
+    for layer in content_layers:
+        content_output.append(vgg19.get_layer(layer).output)
     
-    content_outputs = [vgg19.get_layer(layername).output for layername in content_layers]
-    style_outputs = [vgg19.get_layer(layername).output for layername in style_layers]
-    model_outputs = content_outputs + style_outputs
-   
-    
-    model = Model(vgg19.input,model_outputs)
+    for layer in style_layers:
+        style_output.append(vgg19.get_layer(layer).output)
+
+    output = content_output + style_output
+    model = Model(vgg19.input,output)
     
     return model    
 
@@ -52,7 +51,7 @@ def content_loss(base_content,target):
 
 def gram_matrix(input_tensor):
     channel = int(input_tensor.shape[-1])
-    a = tf.reshape(input_tensor,[-1,channel]) # reshape img into dims of [H*W,C]
+    a = tf.reshape(input_tensor,[-1,channel]) 
     gram = tf.matmul(a,a,transpose_a = True)
     return gram
 
@@ -64,44 +63,25 @@ def style_loss(base_style,target):
     return s_loss
 
 
-def get_feature_representation(model,path,mode):
-    """
-    This helper function will load  picture and the model, 
-    then produce different layer output according to the need
-    
-    Arguments:
-     model : the model we initialized
-     path : path to the picture
-     mode : 'content' feature or 'style' feature
-     
-    Returns:
-     return the content features or the style features
-    """
+def get_feature(model,path,mode):
+
     img = image.pre_process_img(path)
     feature_outputs = model(img)
+    feature_arr = []
+
     if mode == 'style':
-        return [feature[0] for feature in feature_outputs[num_content_layers:]] # get the last several outputs
+        for feature in feature_outputs[num_content_layers:]:
+            feature_arr.append(feature[0])
+       
     if mode =='content':
-        return [feature[0] for feature in feature_outputs[:num_content_layers]] # get the first several outputs
+        for feature in feature_outputs[:num_content_layers]:
+            feature_arr.append(feature[0])
+
+    return feature_arr
     
 
 def loss(model,loss_weights,init_image,content_features,style_features):
-    """
-    The loss function
-    
-    Arguments:
-    
-     model : the model we are using
-     loss_weights : the weights of each contribution in the loss function
-                     (conten tloss weight,style loss weight,vatiation weight)
-     init_image : the generated image upon which we would impose Gradient Descent
-     content_features : the precomputed content picture feature
-     style_features : the precomputed style picture feature
-     
-    Returns:
-    
-     the total loss we are going to optimize
-    """
+
     style_weight,content_weight = loss_weights
     
     # feed the init image in the model,then we would get the 
@@ -115,17 +95,14 @@ def loss(model,loss_weights,init_image,content_features,style_features):
     total_style_loss = 0
     total_content_loss = 0
     
-    # equal seperate the layer weight of style loss
-    # and accumulate the style loss from desired layers
-    weight_per_style_layer = 1.0/ float(num_style_layers)
-    for style_pic_features,gen_pic_stylefeatures in zip(style_features,gen_style_feature):
-        total_style_loss += weight_per_style_layer * style_loss(style_pic_features,gen_pic_stylefeatures)
+    style_layer_weight = 1.0/ float(num_style_layers)
+    for i in range(len(style_features)): 
+        total_style_loss = total_style_loss + style_layer_weight * style_loss(style_features[i], gen_style_feature[i])
     
-    # equal seperate the layer weight of content loss
-    # and accumulate the content loss from desired layers
-    weight_per_content_layer = 1.0/ float(num_content_layers)
-    for content_pic_features,gen_pic_contentfeatures in zip(content_features,gen_content_feature):
-        total_content_loss += weight_per_content_layer * content_loss(content_pic_features,gen_pic_contentfeatures)
+
+    content_layer_weight = 1.0/ float(num_content_layers)
+    for i in range(len(content_features)): 
+        total_content_loss = total_content_loss + content_layer_weight * content_loss(content_features[i], gen_content_feature[i])
     
     total_style_loss *= style_weight
     total_content_loss *= content_weight
@@ -141,20 +118,23 @@ def compute_grads(cfg):
     return tape.gradient(total_loss,cfg['init_image']),allloss
 
 
-def run_nst(content_path,style_path,iteration = 1000,content_weight = 1e3,style_weight = 1):
+def run(content_path,style_path,iteration):
+
+    content_weight = 1e3
+    style_weight = 1
+
     model = model_init()
     for layer in model.layers:
         layer.trainable = False
         
-    content_features = get_feature_representation(model,content_path,mode = 'content')
-    style_features = get_feature_representation(model,style_path,mode = 'style')
+    content_features = get_feature(model, content_path, 'content')
+    style_features = get_feature(model,style_path,'style')
     
     init_image = image.pre_process_img(content_path) # initialize the generated image with content image
     init_image = tf.Variable(init_image,dtype = tf.float32)
     
     opt = tf.keras.optimizers.Adam(5,beta_1 = 0.99,epsilon = 1e-1)
     
-    epoch = 1
     loss_weights = (content_weight,style_weight)
     
     cfg = {
@@ -164,7 +144,7 @@ def run_nst(content_path,style_path,iteration = 1000,content_weight = 1e3,style_
         'content_features':content_features,
         'style_features':style_features
     }
-    
+    #我不太知道这个norm means是怎么来的，image.py里用的也是相同的值，norm means是用来normalize图片的，我看几个github版本用的数值都差不多，但不知道怎么算的
     norm_means = np.array([103.939, 116.779, 123.68])
     min_vals = -norm_means
     max_vals = 255 - norm_means
@@ -173,6 +153,7 @@ def run_nst(content_path,style_path,iteration = 1000,content_weight = 1e3,style_
     best_loss, best_img = float('inf'), None
     imgs = []
     start = datetime.now()
+
     for i in range(iteration):
         
         grads, all_loss = compute_grads(cfg)
@@ -181,7 +162,7 @@ def run_nst(content_path,style_path,iteration = 1000,content_weight = 1e3,style_
         clipped = tf.clip_by_value(init_image, min_vals, max_vals)
         init_image.assign(clipped)
         
-
+        # 以下用了一些image.py里的function，用的github上的代码，之后改掉
         if losss < best_loss:
             # Update best loss and best image from total loss.
             best_loss = losss
@@ -196,13 +177,12 @@ def run_nst(content_path,style_path,iteration = 1000,content_weight = 1e3,style_
                   .format(losss, style_losss, content_losss))
             print(f'100 iters takes {end -start}')
             start = datetime.now()
-        if i % 500 == 0:
-            # Use the .numpy() method to get the concrete numpy array
-            plot_img = init_image.numpy()
-            plot_img = image.deprocess_img(plot_img)
+
+            img = init_image.numpy()
+            img = image.deprocess_img(img)
             path = 'output/output_' + str(i) + '.jpg'
-            image.saveimg(plot_img, path)
-            imgs.append(plot_img)
+            image.saveimg(img, path)
+            imgs.append(img)
 
 
     return best_img, best_loss
--- test.py
+++
@@ -1,8 +0,0 @@
-# Run this py to test if gpu is available
-
-import tensorflow as tf
-from tensorflow.python.client import device_lib
-
-if __name__ == "__main__":
-    print(f'[INFO] GPU AVAILABLE :{tf.test.is_gpu_available()}')
-    print(f'[INFO] AVAILABLE DEVICES :{device_lib.list_local_devices()}')
